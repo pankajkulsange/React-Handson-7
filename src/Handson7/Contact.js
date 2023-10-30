6627f69290cade89dc43773714a77666d8f43526
pankajkulsange <85685465+pankajkulsange@users.noreply.github.com>
2023-10-30 17:07:08 +0530

    Fixed: contact us text styles

--- src/Handson7/Contact.js
+++ src/Handson7/Contact.js
@@ -1,9 +1,9 @@
 import React from "react";
- const Contact=()=>{
-    return(
-        <div>
-            Contact us
-        </div>
-    )
- }
- export default Contact
+const Contact = () => {
+  return (
+    <>
+      <h3 className="home-page">Contact us</h3>
+    </>
+  );
+};
+export default Contact;
